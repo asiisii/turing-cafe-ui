eb9697d7b7129e340e2d87999d3d69b2421a21de
Ashish Malla <ashishmalla45@gmail.com>
2021-05-26 09:52:54 -0600

    Log props to confirm data is getting passed down

---
+++ src/Reservations/Reservations.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Reservations.css'
+
+export default function Reservations({reservations}) {
+  console.log(reservations);
+  return (
+    <h1>in reservations</h1>
+  )
+}
