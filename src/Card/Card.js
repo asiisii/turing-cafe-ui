0f210c9446697cf33d44cc0638f7100715b94bc8
Ashish Malla <ashishmalla45@gmail.com>
2021-05-26 10:11:03 -0600

    Add console to print out the props

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import './Card.css'
 
-export default function Card() {
+export default function Card({name, date, time, number}) {
+  console.log(name, date, time , number);
   return (
     <h1>in Card</h1>
   )
