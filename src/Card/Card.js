80145b1fbe9980b0e0aa1c036231b9315ce0a16c
Ashish Malla <ashishmalla45@gmail.com>
2021-05-26 10:13:17 -0600

    Add p tag to display reservations info on dashboard

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -2,8 +2,12 @@ import React from 'react'
 import './Card.css'
 
 export default function Card({name, date, time, number}) {
-  console.log(name, date, time , number);
   return (
-    <h1>in Card</h1>
+    <div>
+      <p>{name}</p>
+      <p>{date}</p>
+      <p>{time}</p>
+      <p>Number of guests: {number}</p>
+    </div>
   )
 }
