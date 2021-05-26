5888923ec7bc7b330efaaee6b4fe5abf73be531c
Ashish Malla <ashishmalla45@gmail.com>
2021-05-26 10:23:20 -0600

    Add classname to card div

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -3,8 +3,8 @@ import './Card.css'
 
 export default function Card({name, date, time, number}) {
   return (
-    <div>
-      <p>{name}</p>
+    <div className='card'>
+      <p className='name'>{name}</p>
       <p>{date}</p>
       <p>{time} pm</p>
       <p>Number of guests: {number}</p>
