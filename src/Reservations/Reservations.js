0b78326a8b8bade0e2fcabee44401ca3f2d1e04f
Ashish Malla <ashishmalla45@gmail.com>
2021-05-26 10:05:54 -0600

    Add map method to console to see each data

--- src/Reservations/Reservations.js
+++ src/Reservations/Reservations.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Card from '../Card/Card'
 import './Reservations.css'
 
 export default function Reservations({reservations}) {
   console.log(reservations);
+  const cards = reservations.map(reservation => {
+    console.log(reservation);
+  })
   return (
-    <h1>in reservations</h1>
+    <h1>hi</h1>
   )
 }
